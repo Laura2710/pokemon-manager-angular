2ed7c79b255955e55ab9baf60f6ef673671f6c15
Laura TOMASINI <tomasinilaura1988@gmail.com>
2024-07-05 10:33:24 +0200

    refactorisation

--- src/app/composants/generations/generations.component.ts
+++ src/app/composants/generations/generations.component.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {Component, Inject, OnInit} from '@angular/core';
 import {PokemonService} from '../../services/pokemon.service';
 import {RouterLink} from "@angular/router";
-import {map, Observable} from "rxjs";
+import {map, Observable, of} from "rxjs";
 import {Generation} from "../../models/Generation";
 
 @Component({
@@ -14,18 +14,22 @@ import {Generation} from "../../models/Generation";
 })
 export class GenerationsComponent implements OnInit {
   generations$: Observable<Generation[]> | undefined;
-
+  generations: Generation[] = [];
 
   constructor(@Inject(PokemonService) private pokemonService: PokemonService) {
   }
 
   ngOnInit(): void {
-
+    this.obtenirGenerationsPokemon()
   }
 
   obtenirGenerationsPokemon() {
     this.generations$ = this.pokemonService.chargerGenerations().pipe(
-      map((data: any) => data.results))
+      map((data: any) => {
+        return data.results;
+      })
+    )
   }
 
+  protected readonly of = of;
 }
--- src/app/models/Generation.ts
+++ src/app/models/Generation.ts
@@ -1,3 +1,4 @@
 export interface Generation {
   name: string;
+  url: string;
 }
